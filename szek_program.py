7ac8bf81a25102d33453499c3c5e99c4d6f3c13e
viktoriaszalkai <viktoriaszalkai04@gmail.com>
2024-01-23 13:31:27 +0100

    szekProgramMetodusokkal

--- szek_program.py
+++ szek_program.py
@@ -1,7 +1,7 @@
 from Szek import Szek
 
 peldany1 = Szek("kék", 3)
-peldany2 = Szek("piros", 4)
+peldany2 = Szek("piros", 5)
 peldany3 = Szek("zöld", 5)
 
 print(peldany1.__str__())
@@ -10,20 +10,8 @@ print(peldany3.__str__())
 
 szekek=[peldany1, peldany2, peldany3]
 
-def labakSzama():
-    print("Összesen hány székláb van a teremben: ", end="")
-    ossz=0
-    for index in range(0, len(szekek),1):
-        ossz += szekek[index].labszam
-    print(f"{ossz} db")
 
-labakSzama()
 
-def maxLabSzine():
-    maxLabIndex = 0
-    for index in range(1, len(szekek), 1):
-        if szekek[maxLabIndex].labszam > szekek[index].labszam:
-            maxLabIndex=index
-    return szekek[maxLabIndex].szin
 
-print(f"A legtöbb lábú szék színe: {maxLabSzine()}")
+
+
