a5235a42bdd3fd020991fb904cda8e7e24d6bef8
viktoriaszalkai <viktoriaszalkai04@gmail.com>
2024-01-23 11:55:53 +0100

    eddigi anyagok

--- osszegzes.py
+++ osszegzes.py
@@ -1,11 +1,7 @@
 n = -1
 while n < 0:
-    n = int(input("N="))
+    n = int(input("N= "))
 ossz = 0
-i = 1
-for i in range(i, n+1, 1):
+for i in range(1, n + 1, 1):
     ossz += i
-print(f"Az első {n}db természetes szám összege: {ossz}")
-
-
-
+print(f"Az első {n} db természetes szám összege: {ossz}")
